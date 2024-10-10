58c9f693331eaac8dda3ebffe3b77fa8b0903b52
Muhammad Nadeem <127011618+Nadeem1607@users.noreply.github.com>
2024-10-10 15:51:34 +0530

    Update Telegram_Bot.py

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -51,7 +51,7 @@ async def fetch_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"Failed to connect to {ip}:{port}. Error: {str(e)}")
 
 def main():
-    application = ApplicationBuilder().token('7776623462:AAEYYt5rpNtwEBwSy-Pup0fGl1Mna7N8eoU').build()
+    application = ApplicationBuilder().token('<Your_BOT_Token>').build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("setconnection", set_connection))
